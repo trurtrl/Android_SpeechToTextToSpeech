f75443d4de0545afa895efb260809d56f28bd973
trurtrl <trurtrl@gmail.com>
2019-07-20 14:20:20 +0300

    Speech to Text

--- app/src/main/java/com/example/speechtotexttospeech/MainActivity.java
+++ app/src/main/java/com/example/speechtotexttospeech/MainActivity.java
@@ -1,21 +1,43 @@
 package com.example.speechtotexttospeech;
 
+import android.Manifest;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.os.Build;
+import android.speech.RecognitionListener;
+import android.speech.RecognizerIntent;
+import android.speech.SpeechRecognizer;
 import android.speech.tts.TextToSpeech;
+import android.support.annotation.NonNull;
+import android.support.constraint.ConstraintLayout;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.MotionEvent;
 import android.view.View;
+import android.view.inputmethod.InputMethodManager;
 import android.widget.Button;
+import android.widget.ImageButton;
 import android.widget.EditText;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
 import android.widget.Toast;
 
+import java.util.ArrayList;
 import java.util.Locale;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
     private TextToSpeech textToSpeech;
     private Button mButtonPronounce;
     private EditText mEditTextToPronounce;
+
+    private ImageButton mButtonRecord;
+    private TextView mTextViewRecord;
+
+    private SpeechRecognizer mSpeechRecognizer;
+    private Intent mSpeechRecognizerIntent;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -43,6 +65,37 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+        mButtonRecord = (ImageButton) findViewById(R.id.mButtonRecord);
+        mTextViewRecord = (TextView) findViewById(R.id.mTextViewRecord);
+
+        checkPermissions();
+
+        findViewById(R.id.mButtonRecord).setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View view, MotionEvent motionEvent) {
+                switch (motionEvent.getAction()) {
+                    case MotionEvent.ACTION_UP:
+                        //when the user removed the finger
+                        mTextViewRecord.setHint("You will see input here...");
+                        mSpeechRecognizer.stopListening();
+                        break;
+
+                    case MotionEvent.ACTION_DOWN:
+                        //finger is on the button
+                        mTextViewRecord.setText("");
+                        mTextViewRecord.setHint("Listening...");
+                        mSpeechRecognizer.startListening(mSpeechRecognizerIntent);
+                        break;
+                }
+                return false;
+            }
+        });
+
+        ConstraintLayout layout = (ConstraintLayout) findViewById(R.id.mBackgroundLayout);
+        layout.setOnClickListener(this);
+        TextView textViewQuestion = (TextView) findViewById(R.id.textView);
+        textViewQuestion.setOnClickListener(this);
+        mTextViewRecord.setOnClickListener(this);
     }
 
     public void OnPronounce(View view) {
@@ -61,6 +114,105 @@ public class MainActivity extends AppCompatActivity {
         if (textToSpeech != null) {
             textToSpeech.stop();
             textToSpeech.shutdown();
+        }
+    }
+
+    private void checkPermissions() {
+        //  if Android version is Marshmallow or ahead
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            if (ContextCompat.checkSelfPermission(this, Manifest.permission.RECORD_AUDIO) == PackageManager.PERMISSION_GRANTED) {
+                mTextViewRecord.setText("");
+                startSpeechRecognizer();
+            } else {
+                requestPermissions(new String[]{Manifest.permission.RECORD_AUDIO}, 1);
+                //mButtonRecord.setVisibility(View.INVISIBLE);
+            }
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if (requestCode == 1) {
+            if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                mTextViewRecord.setText("");
+                startSpeechRecognizer();
+            } else {
+                mButtonRecord.setVisibility(View.INVISIBLE);
+            }
+        }
+    }
+
+
+    private void startSpeechRecognizer() {
+        mSpeechRecognizerIntent = new Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH);
+        mSpeechRecognizerIntent.putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL,
+                RecognizerIntent.LANGUAGE_MODEL_FREE_FORM);
+        mSpeechRecognizerIntent.putExtra(RecognizerIntent.EXTRA_LANGUAGE,
+                Locale.US);
+
+        mSpeechRecognizer = SpeechRecognizer.createSpeechRecognizer(this.getApplicationContext());
+
+        mSpeechRecognizer.setRecognitionListener(new RecognitionListener() {
+            @Override
+            public void onReadyForSpeech(Bundle bundle) {
+
+            }
+
+            @Override
+            public void onBeginningOfSpeech() {
+
+            }
+
+            @Override
+            public void onRmsChanged(float v) {
+
+            }
+
+            @Override
+            public void onBufferReceived(byte[] bytes) {
+
+            }
+
+            @Override
+            public void onEndOfSpeech() {
+
+            }
+
+            @Override
+            public void onError(int i) {
+
+            }
+
+            @Override
+            public void onResults(Bundle bundle) {
+                //getting all the matches
+                ArrayList<String> matches = bundle
+                        .getStringArrayList(SpeechRecognizer.RESULTS_RECOGNITION);
+
+                //displaying the first match
+                if (matches != null)
+                    mTextViewRecord.setText(matches.get(0));
+            }
+
+            @Override
+            public void onPartialResults(Bundle bundle) {
+
+            }
+
+            @Override
+            public void onEvent(int i, Bundle bundle) {
+
+            }
+        });
+    }
+
+    // hide a keyboard
+    @Override
+    public void onClick(View view) {
+        if (view.getId() != R.id.mEditTextToPronounce) {
+            InputMethodManager inputMethodManager = (InputMethodManager) getSystemService(INPUT_METHOD_SERVICE);
+            inputMethodManager.hideSoftInputFromWindow(getCurrentFocus().getWindowToken(), 0);
         }
     }
 }
